9ca2b6d55ee54e662b40ec334ff86a04c93df956
dgross881 <dgross881@gmail.com>
2015-08-21 17:54:38 +0800

    fixed javascript functions

--- app/controllers/spree/admin/product_imports_controller.rb
+++ app/controllers/spree/admin/product_imports_controller.rb
@@ -4,8 +4,8 @@ module Spree
       def create
         @product_import = Spree::ProductImport.create(csv_import_params)
         if @product_import.save
-          @product_import.add_products!
-          #add_or_update_products
+          add_or_update_products
+
           flash[:success] = "You have successfuly Imported products" 
           redirect_to admin_products_path
         else 
@@ -17,6 +17,7 @@ module Spree
 
     private 
 
+    
       def add_or_update_products
         if @product_import.preferred_update_products
           @product_import.update_products!
@@ -26,7 +27,7 @@ module Spree
       end 
 
       def csv_import_params
-        params.fetch(:product_import, {}).permit(:csv_import, :preferred_translate_products)
+        params.fetch(:product_import, {}).permit(:csv_import, :preferred_add_products, :preferred_update_products)
       end 
     end 
   end 
--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -8,6 +8,9 @@ class Spree::ProductImport < Spree::Base
   has_attached_file :csv_import, :path => ":rails_root/lib/etc/product_data/data-files/:basename.:extension"
   validates_attachment :csv_import, presence: true, :content_type => { content_type: ['application/excel','application/vnd.ms-excel','application/vnd.msexcel', "application/pdf","application/vnd.ms-excel"  , "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]} 
 
+  validates_inclusion_of :preferred_add_products, in: [true], if: lambda {|u| !u.preferred_update_products }
+  validates_inclusion_of :preferred_update_products, in: [true], if: lambda {|u| !u.preferred_add_products }
+
 
   def add_products!
     import_products
@@ -49,7 +52,7 @@ class Spree::ProductImport < Spree::Base
     import_products
     header = @products_csv.row(1)
     (3..@products_csv.last_row).each do |row| 
-     product = Spree::ImportProduct.new(Hash[[header.map(&:to_sym), @products_csv.row(row)].transpose]) 
+       product = Spree::ImportProduct.new(Hash[[header.map(&:to_sym), @products_csv.row(row)].transpose]) 
       if product.slug.present? 
         update_product = Spree::Product.find_by(slug: product.slug)
         update_product.update_attributes(clean_product(product))
@@ -111,7 +114,7 @@ class Spree::ProductImport < Spree::Base
   end 
 
   def clean_product(product)
-    variant.instance_values.symbolize_keys.reject {|key, value| !Spree::Product.attribute_method?(key) || value.nil? }
+    product.instance_values.symbolize_keys.reject {|key, value| !Spree::Product.attribute_method?(key) || value.nil? }
   end 
 
   def find_property(property)
@@ -127,6 +130,9 @@ class Spree::ProductImport < Spree::Base
   end
 
   def add_translations(new_product, product) 
-     new_product.update_attributes(name: product.name_cn, description: product.description_cn, locale: :cn)
+     translations = product.instance_values.select { |key, value| key.match(/_cn/) && value.present? }
+     if translations
+       new_product.update_attributes(name: product.name_cn, description: product.description_cn, locale: :cn)
+     end
   end
 end
